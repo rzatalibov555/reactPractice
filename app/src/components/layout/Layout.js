ad9ebf831ca0f7320fb7cef7a70bf78b2e3134c6
rzatalibov555 <sethubpro@gmail.com>
2024-02-28 13:14:58 +0400

    create layout using HOC

---
+++ app/src/components/layout/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./layout.css"
+import {AddClass} from '../../hoc/AddClass'
+
+const Layout = () =>{
+
+    return(
+        <h1>Layout</h1>
+    )
+
+}
+
+export default AddClass(Layout, 'layout')
